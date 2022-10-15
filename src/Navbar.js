04b283551f90ea9584e23a55241b44b93508749c
Shahjalal Sikder <shah.sikder55@gmail.com>
2022-10-15 14:38:38 -0400

    Created basic layout of Navbar without routing

---
+++ src/Navbar.js
@@ -0,0 +1,20 @@
+export default function Navbar() {
+    return (
+        <nav className="nav">
+            <a href="/" className="site-title">
+                BookShare
+            </a>
+            <ul>
+                <li className="active">
+                    <a href="/about">About</a>
+                </li>
+                <li>
+                    <a href="/profile">Profile</a>
+                </li>
+                <li>
+                    <a href="/settings">Settings</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
