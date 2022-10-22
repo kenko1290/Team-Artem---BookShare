206dcde655d493216be1d45567f3ee56ad931970
Shahjalal Sikder <shah.sikder55@gmail.com>
2022-10-22 17:00:22 -0400

    Added search bar to nav

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,9 +4,16 @@ export default function Navbar() {
             <a href="/" className="site-title">
                 BookShare
             </a>
+            <SearchBar />
             <ul>
                 <li className="active">
                     <a href="/about">About</a>
+                </li>
+                <li>
+                    <a href="/home">Home</a>
+                </li>
+                <li>
+                    <a href="/my-account">My Account</a>
                 </li>
                 <li>
                     <a href="/profile">Profile</a>
@@ -18,6 +25,22 @@ export default function Navbar() {
         </nav>
     )
 }
+
+const SearchBar = () => (
+    <form action="/" method="get" className="nav-search-bar">
+        <label htmlFor="header-search">
+            <span className="visually-hidden">Search</span>
+        </label>
+        <input
+            type="text"
+            id="header-search"
+            placeholder="Find books..."
+            name="s"
+        />
+        <button type="submit">Search</button>
+    </form>
+);
+
 
 //Add a post button and rest of navbar components
 //Add search bar
