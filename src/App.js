082e582f27647b091101d6988541d46d1cb2b07b
wenbin1354 <wblin2986@gmail.com>
2022-10-23 11:23:25 -0400

    get rid of edit_profile

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import "./App.css";
 import ShowBookInfo from "./book_info/book_information"
-import EditProfile from "./edit_profile";
 
 function App() {
 	return (
 		<div className="App">
-			{/* <ShowBookInfo />  */}
-			<EditProfile />
+			<ShowBookInfo />
+			
 		</div>
 	);
 }
