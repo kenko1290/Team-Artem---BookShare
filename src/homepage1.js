220c57a599d0fbf3a0e6b849522ad86bf8cb67de
Shahjalal Sikder <shah.sikder55@gmail.com>
2022-10-29 14:44:24 -0400

    Initial commit

---
+++ src/homepage1.js
@@ -0,0 +1,16 @@
+export default function HomePage1() {
+    return (
+        <>
+            <nav className="nav">
+                <a href="/" className="site-title">
+                    BookShare
+                </a>
+                <ul>
+                    <li className="active">
+                        <a href="/about">About</a>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    )
+}
