6f3e63d1ab7fc815d312f41d6a95c3ae566d79ce
Oumar Siby <oumarsiby10@gmail.com>
2022-10-30 10:19:29 -0400

    made final edits to sidebar

--- src/SideBar/sideBar.js
+++ src/SideBar/sideBar.js
@@ -1,23 +1,23 @@
 import React from "react";
 import "./sideBar.css"
-import ProfileCard from "../UserProfileCard/profileCard";
+import ProfileCard from "./UserProfileCard/profileCard";
 
 function SideBar(){
-    let nav = "profile";
+    let nav = "bookPage";
     const homePageSideBar = 
     <div className="sideBarContainer">
             <h5>Filter By Classes</h5>
-            <button class="d-block">class1</button>
-            <button class="d-block">class2</button>
-            <button class="d-block">class3</button>
+            <button className="d-block">class1</button>
+            <button className="d-block">class2</button>
+            <button className="d-block">class3</button>
 
             <h5>Filter By Availability</h5>
-            <button class="d-block">available</button>
-            <button class="d-block">not available</button>
+            <button className="d-block">available</button>
+            <button className="d-block">not available</button>
 
             <h5>Filter By Sharing Type</h5>
-            <button class="d-block">donated Books</button>
-            <button class="d-block">lent Books</button>
+            <button className="d-block">donated Books</button>
+            <button className="d-block">lent Books</button>
 
             <button>Clear Filter</button>
     </div>
@@ -25,62 +25,62 @@ function SideBar(){
     const postPageSideBar = 
     <div className="sideBarContainer">
         <h5>Type Of Sharing</h5>
-        <button class="d-block">Lend Book</button>
-        <button class="d-block">Donate Book</button>
+        <button className="d-block">Lend Book</button>
+        <button className="d-block">Donate Book</button>
     </div>
     
     const requestsPageSideBar = 
     <div className="sideBarContainer">
         <h5>Requests</h5>
-        <button class="d-block"> My Requests For Books</button>
-        <button class="d-block"> Requests For My Books  </button>
+        <button className="d-block"> My Requests For Books</button>
+        <button className="d-block"> Requests For My Books  </button>
     </div>
     
     const myAccountPageSideBar = 
     <div className="sideBarContainer"> 
         <h5>My Account</h5>
-        <button class="d-block">Boooks Burrowed</button>
-        <button class="d-block">Books Owned</button>
-        <button class="d-block">Books Lent</button>
-        <button class="d-block">Books Donated</button>
-        <button class="d-block">Books Saved</button>
-        <button class="d-block">Messages</button>
+        <button className="d-block">Boooks Burrowed</button>
+        <button className="d-block">Books Owned</button>
+        <button className="d-block">Books Lent</button>
+        <button className="d-block">Books Donated</button>
+        <button className="d-block">Books Saved</button>
+        <button className="d-block">Messages</button>
     </div>
 
     const bookPageSideBar = 
     <div className="sideBarContainer">
         <div className="imageContainer">
-            <img src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png" alt="book" />
+            <img src="https://d827xgdhgqbnd.cloudfront.net/wp-content/uploads/2016/04/09121712/book-cover-placeholder.png" alt="book" />
         </div>
 
-        <button class="d-block">Request Book</button>
+        <button className="d-block">Request Book</button>
 
-        <button class="d-block">Save Book</button>
+        <button className="d-block">Save Book</button>
 
         <h5>Owner</h5>
         <ProfileCard />
 
-        <button class="d-block">Message Owner</button>
+        <button className="d-block">Message Owner</button>
 
     </div>
     
     const settingsPageSideBar = 
     <div className="sideBarContainer">
         <h5>Settings</h5>
-        <button class="d-block">General</button>
-        <button class="d-block">Privacy</button>
+        <button className="d-block">General</button>
+        <button className="d-block">Privacy</button>
     </div>
     const progfilePageSideBar = 
     <div className="sideBarContainer">
         <ProfileCard />
-        <button class="d-block">Edit Profile</button>
+        <button className="d-block">Edit Profile</button>
     </div>
 
     const profileEditsPageSideBar = 
     <div className="sideBarContainer">
         <ProfileCard />
         <button>Change Image</button>
-        <button class="d-block">Save Changes</button>
+        <button className="d-block">Save Changes</button>
     </div>
 
     if(nav === "home")
