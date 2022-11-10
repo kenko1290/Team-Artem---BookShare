d8d4e948032bebd936e24327feb68974d89a6f79
Ken Ko <ken.ko1290@gmail.com>
2022-11-09 20:06:44 -0500

    Revert "Merge pull request #5 from kenko1290/About-Us-Page"
    This reverts commit 70b2998c926a994e1c6231b858718eff69dd611d, reversing
    changes made to 9aa721551c8f90121c73b628dcbed5c03e40f0ef.

---
+++ src/homepage1.js
@@ -0,0 +1,13 @@
+import SmallNav from './SmallNav.js';
+
+export default function HomePage1() {
+    return (
+        <>
+            <SmallNav />
+            <div className="btns">
+                <button className="login-btn">Login</button>
+                <button className="sign-up">Create an Account</button>
+            </div>
+        </>
+    )
+}
