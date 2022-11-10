85d06d52ebd3d377ec6580eee0e6616950782485
Ken Ko <ken.ko1290@gmail.com>
2022-11-09 19:18:11 -0500

    updated sidebar border

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,6 +1,7 @@
 //import classes from "./PostPage.module.css";
 import Navbar from "../components/Navbar";
 import PostBookForm from "../components/PostBookForm";
+import SideBar from "../components/SideBar/sideBar";
 
 function PostPage() {
   return (
@@ -8,7 +9,7 @@ function PostPage() {
       <Navbar />
       <div class="container-fluid">
         <div class="row mb-4">
-          <div class="col-2 fw-bold border-right text-center">Replace with Sidebar Component</div>
+          <div class="col-2 fw-bold">{<SideBar/>}</div>
           <div class="col border border-dark border-bottom-0 border-top-0 border-bottom-right-0" style={{paddingLeft:"50px", paddingTop:"20px"}}>{<PostBookForm />}</div>
         </div>
       </div>
